20cd1f93ff834a7699d861f930d0007fc66c9ba8
Jim Lii <jim.li@mbowebshop.nl>
2024-07-31 01:40:35 +0200

    add illustration page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,17 +1,21 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
-    {
-        path: 'design',
-        loadComponent: () => import('./components/design/design.component').then(m => m.DesignComponent)
-    },
-      {
-        path: 'about',
-        loadComponent: () => import('./components/about/about.component').then(m => m.AboutComponent)
-    },
-      {
-        path: '',
-        redirectTo: 'design',
-        pathMatch: 'full',
-      },
+  {
+    path: 'design',
+    loadComponent: () => import('./components/design/design.component').then(m => m.DesignComponent)
+  },
+  {
+    path: 'illustration',
+    loadComponent: () => import('./components/illustration/illustration.component').then(m => m.IllustrationComponent)
+  },
+  {
+    path: 'about',
+    loadComponent: () => import('./components/about/about.component').then(m => m.AboutComponent)
+  },
+  {
+    path: '',
+    redirectTo: 'design',
+    pathMatch: 'full',
+  },
 ]
---
+++ src/app/components/illustration/illustration.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { NavbarComponent } from '../shared/navbar/navbar.component';
+
+@Component({
+  selector: 'app-illustration',
+  standalone: true,
+  imports: [NavbarComponent],
+  templateUrl: './illustration.component.html',
+  styleUrl: './illustration.component.css'
+})
+export class IllustrationComponent {
+
+}
