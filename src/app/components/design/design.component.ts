a2ab614900cd6dbbe05d9be16511c622038777dc
Jim Lii <jim.li@mbowebshop.nl>
2024-07-31 21:55:40 +0200

    add footer

--- src/app/components/design/design.component.ts
+++ src/app/components/design/design.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { Router, RouterLink, RouterModule } from '@angular/router';
 import { NavbarComponent } from '../shared/navbar/navbar.component';
+import { FooterComponent } from '../shared/footer/footer.component';
 
 @Component({
   selector: 'app-design',
   standalone: true,
-  imports: [NavbarComponent],
+  imports: [NavbarComponent, FooterComponent],
   templateUrl: './design.component.html',
   styleUrl: './design.component.css'
 })
