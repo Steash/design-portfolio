9cb26e3f3a2800ce07b1aa0a5ee541dfdcd66299
Jim Lii <jim.li@mbowebshop.nl>
2024-10-25 14:43:49 +0200

    add carousel buttons

--- src/app/components/design/design.component.ts
+++ src/app/components/design/design.component.ts
@@ -13,6 +13,7 @@ import { Design } from '../../models/design.model';
 export class DesignComponent {
   designs: Design[] = [
     {
+      id: 1,
       title: 'Little Dots',
       description: `The album artwork for Little Dots’ “Do you know how we got here”, featuring a vinyl edition and a four-panel digi pack, is a minimalistic design inspired by connect-the-dots puzzles, inviting the viewer to engage with the band's transformative journey. The dots form a path through an open space, encouraging the audience to "connect the dots" and find their own meaning. This interactive simplicity mirrors the band's new direction, where authenticity and vulnerability meet bold exploration, offering an invitation to discover their evolving sound.<br>Collaboratively crafted with Studio Studio.`,
       imageGroup: [
@@ -26,6 +27,7 @@ export class DesignComponent {
       ]
     },
     {
+      id: 2,
       title: 'I AM WHO I AM',
       description: `Poster en handout for the 11th annual group exhibition of i-psy’s Open Atelier programme “I AM WHO I AM”. The exhibition not only reflects the diversity of the artists and their individual histories, it also reflects the individual ways in which the artists have chosen to express their creative therapeutic process. WHO I AM speaks to the imperative to accept one’s self. To claim and reclaim one’s space as a creative being, to demand the right to be who you are.`,
       imageGroup: [
@@ -39,6 +41,7 @@ export class DesignComponent {
       ]
     },
     {
+      id: 3,
       title: 'CCTV in Buikslotermeerplein',
       description: `To increase security and to combat nuisance in public space, the municipality has taken a number of measures <br><br> including the use of CCTV. There are 18 camera areas in Amsterdam, one of them is Buikslotermeerplein. The <br><br> cameras give police and enforcement an ‘extra eyes’ in the city. Many residents have reported to feel safer with <br><br> CCTV, even if it means they must give up their privacy. There is a total of 68 CCTV in Buikslotermeerplein, I captured them all and provided each with the exact location.`,
       imageGroup: [
@@ -53,6 +56,7 @@ export class DesignComponent {
       ]
     },
     {
+      id: 4,
       title: 'Another Graphic',
       description: `Another Graphic is a digital archive focusing on typographic treatment within graphic design practices. At the heart of the identity is the concept of archiving, visually represented through a grid-based system that underpins every design element. Inspired by the clarity and precision of mathematical and scientific graphics, the brand identity reflects Another Graphic’s mission to be a precise and reliable resource for typographic exploration. While also positioning the archive as a space where design meets analysis, creativity meets calculation and where every element is meticulously archived for future reference.`,
       imageGroup: [
@@ -65,6 +69,7 @@ export class DesignComponent {
       ]
     },
     {
+      id: 5,
       title: 'Studio Studio',
       description: `Branding essentials for Studio Studio which include a door poster, tote bag, and business card. Together, these pieces create a unified and memorable brand experience, reflecting Studio Studio’s expertise in clear and impactful visual communication. Each item is designed to resonate with clients and visitors, ensuring that the studio’s identity is consistently conveyed across all touchpoints.`,
       imageGroup: [
@@ -75,6 +80,7 @@ export class DesignComponent {
       ]
     },
     {
+      id: 6,
       title: 'Depend On Me',
       description: `Poster and handout for Amsterdam-based research group “depend on me”. The group brings together artists from different contexts, levels and generations who are related through similar interests, questions and collaborative projects. The research program explores the boundaries between professional and amateur artists and show that a certain degree of dependence within an artistic process can also be powerful and serve as a source of inspiration.`,
       imageGroup: [
@@ -87,6 +93,7 @@ export class DesignComponent {
       ]
     },
     {
+      id: 7,
       title: 'Mutant Garden',
       description: `Booklet for Harm van den Dorpel's second exhibition “Mutant Garden” at Upstream Gallery in Amsterdam.`,
       imageGroup: [
@@ -101,6 +108,7 @@ export class DesignComponent {
       ]
     },
     {
+      id: 8,
       title: 'Proud Aliens',
       description: `The project group of Framer Framed’s Open Atelier have created an utopian future where difference and diversity are recognised as beautiful and celebrated. The exhibition’s identity reflects the diverse cultural backgrounds of the participants.`,
       imageGroup: [
@@ -109,6 +117,7 @@ export class DesignComponent {
       ]
     },
     {
+      id: 9,
       title: 'HKU Fashion & Design Graduation Show',
       description: `The identity for the Fashion Department’s graduation show at the Art Academy of Utrecht is a striking embodiment of innovation and creativity. It seamlessly blends all the students' artworks into a fluid, cohesive design, much like watercolors merging together—each piece maintaining its individuality while contributing to a vibrant whole. This concept reflects the students' bold approach to their craft, where they push boundaries, embrace experimentation, and redefine the norms of fashion design.<br><br>Collaboratively crafted with Sanne Bouman.`,
       imageGroup: [
@@ -127,6 +136,7 @@ export class DesignComponent {
       ]
     },
     {
+      id: 10,
       title: 'Doe Een Wens',
       description: `The poster for the group exhibition “Doe Een Wens” highlights a diverse array of artists, each interpreting their dreams through various artistic mediums. The design visually conveys the transformative journey from imagination to art, inviting viewers to explore the varied and vibrant dreams captured in the show.`,
       imageGroup: [
