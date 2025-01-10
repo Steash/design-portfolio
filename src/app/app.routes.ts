ff5998dc7a2310b9b70d8844dfddceba22c4c878
Jim Lii <jim.li@mbowebshop.nl>
2025-01-10 14:02:11 +0100

    add swiper js

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -9,12 +9,8 @@ export const routes: Routes = [
     path: 'about',
     loadComponent: () => import('./components/about/about.component').then(m => m.AboutComponent)
   },
-  // {
-  //   path: '',
-  //   loadComponent: () => import('./components/design/design.component').then(m => m.DesignComponent)
-  // },
   {
     path: '',
-    loadComponent: () => import('./components/test-swiper/test-swiper.component').then(m => m.TestSwiperComponent)
+    loadComponent: () => import('./components/design/design.component').then(m => m.DesignComponent)
   },
 ]
